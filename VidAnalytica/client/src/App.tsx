327ef2c4e665caa6922b11d8ccbaf819e77c280f
azeez <devops@ittech.com>
2025-07-03 04:18:02 +0300

    fix url

--- VidAnalytica/client/src/App.tsx
+++ VidAnalytica/client/src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { HashRouter as Router, Routes, Route } from "react-router-dom"
 import { ThemeProvider } from "./components/ui/theme-provider"
 import { Toaster } from "./components/ui/toaster"
 import { AuthProvider } from "./contexts/AuthContext"
@@ -17,27 +17,27 @@ import { Settings } from "./pages/Settings"
 
 function App() {
   return (
-  <AuthProvider>
-    <ThemeProvider defaultTheme="light" storageKey="ui-theme">
-      <Router>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/" element={<ProtectedRoute> <Layout /> </ProtectedRoute>}>
-            <Route index element={<Dashboard />} />
-            <Route path="channels" element={<Channels />} />
-            <Route path="videos" element={<Videos />} />
-            <Route path="transcripts" element={<Transcripts />} />
-            <Route path="ideas" element={<Ideas />} />
-            <Route path="analytics" element={<Analytics />} />
-            <Route path="settings" element={<Settings />} />
-          </Route>
-          <Route path="*" element={<BlankPage />} />
-        </Routes>
-      </Router>
-      <Toaster />
-    </ThemeProvider>
-  </AuthProvider>
+    <AuthProvider>
+      <ThemeProvider defaultTheme="light" storageKey="ui-theme">
+        <Router>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/" element={<ProtectedRoute><Layout /></ProtectedRoute>}>
+              <Route index element={<Dashboard />} />
+              <Route path="channels" element={<Channels />} />
+              <Route path="videos" element={<Videos />} />
+              <Route path="transcripts" element={<Transcripts />} />
+              <Route path="ideas" element={<Ideas />} />
+              <Route path="analytics" element={<Analytics />} />
+              <Route path="settings" element={<Settings />} />
+            </Route>
+            <Route path="*" element={<BlankPage />} />
+          </Routes>
+        </Router>
+        <Toaster />
+      </ThemeProvider>
+    </AuthProvider>
   )
 }
 
